f238413769d8884386861d10409732b93eed9006
Pei XY <pei.xy0@outlook.com>
2015-05-03 01:14:37 +0800

    F# hacker news fetcher

---
+++ HackerNewsFetcher.fs
@@ -0,0 +1,21 @@
+﻿// Fetch hacker news top stories with Hacker News API
+// https://github.com/HackerNews/API
+
+open FSharp.Data
+
+type TopStories = JsonProvider<"https://hacker-news.firebaseio.com/v0/topstories.json">
+type Story = JsonProvider<"https://hacker-news.firebaseio.com/v0/item/1.json">
+
+let getStory id =
+    sprintf "https://hacker-news.firebaseio.com/v0/item/%d.json" id
+    |> Story.AsyncLoad
+
+[<EntryPoint>]
+let main argv =
+    TopStories.GetSamples()
+    |> Seq.truncate 10
+    |> Seq.map getStory
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.iter (fun t -> printfn "%s" t.Title)
+    0
